1709c5b72957a878b9f0ea14e850445b7eab9282
QueenObehi <71190922+QueenObehi@users.noreply.github.com>
2021-01-10 12:28:40 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,27 @@
+document.querySelector('#red').onclick = function () {
+    document.querySelector('body').style.backgroundColor = 'crimson';
+}
+document.querySelector('#orange').onclick = function () {
+    document.querySelector('body').style.backgroundColor = 'darkorange';
+}
+document.querySelector('#yellow').onclick = function () {
+    document.querySelector('body').style.backgroundColor = 'yellow';
+}
+document.querySelector('#green').onclick = function () {
+    document.querySelector('body').style.backgroundColor = 'limegreen';
+}
+document.querySelector('#blue').onclick = function () {
+    document.querySelector('body').style.backgroundColor = 'cornflowerblue';
+}
+document.querySelector('#indigo').onclick = function () {
+    document.querySelector('body').style.backgroundColor = 'rebeccapurple';
+}
+document.querySelector('#violet').onclick = function () {
+    document.querySelector('body').style.backgroundColor = 'mediumorchid';
+}
+document.querySelector('#black').onclick = function () {
+    document.querySelector('body').style.backgroundColor = 'darkslategrey';
+}
+document.querySelector('#white').onclick = function () {
+    document.querySelector('body').style.backgroundColor = 'white';
+}
